7de6ff6c5d2e035ca85ecceb2f6ddda2d2c8a9f5
DavidSoto5317 <urielsonic@gmail.com>
2020-03-25 15:06:01 -0600

    Se agrega un boton para opciones de usuario

--- bankfy_app/lib/screens/MainScreen/main_screen.dart
+++ bankfy_app/lib/screens/MainScreen/main_screen.dart
@@ -24,6 +24,21 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     // final ScreenArguments args = ModalRoute.of(context).settings.arguments;
     // final user = Provider.of<User>(context);
+    void _showSettingsPanel() {
+      showModalBottomSheet(context: context, builder: (context) { 
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          child: FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Salir'),
+            onPressed: () async {
+              await _auth.signOut();
+              Navigator.pop(context);
+            },
+          ), 
+        );
+      });
+    }
 
     return StreamProvider<QuerySnapshot>.value(
       value: DatabaseService().datos,
@@ -35,12 +50,11 @@ class _MainScreenState extends State<MainScreen> {
           elevation: 0.0,
           actions: <Widget>[
             FlatButton.icon(
-                icon: Icon(Icons.person),
-                label: Text('Salir'),
-                onPressed: () async {
-                  await _auth.signOut();
-                },
-              )
+              icon: Icon(Icons.settings),
+              padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+              label: Text(''),
+              onPressed: () => _showSettingsPanel(),
+            ),           
           ],
         ),
       ),
