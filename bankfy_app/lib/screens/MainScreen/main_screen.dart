6fd100e1f2bb1dfb3d963c079459fe72ca432df7
DavidSoto5317 <urielsonic@gmail.com>
2020-03-25 16:46:22 -0600

    Se termina la ventana de menu principal

--- bankfy_app/lib/screens/MainScreen/main_screen.dart
+++ bankfy_app/lib/screens/MainScreen/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bankfyapp/screens/LoginScreen/login_screen.dart';
 import 'package:bankfyapp/services/auth.dart';
 import 'package:bankfyapp/services/database.dart';
 import 'package:bankfyapp/screens/MainScreen/texto_nombre_usuario.dart';
@@ -19,6 +20,142 @@ class ScreenArguments {
 
 class _MainScreenState extends State<MainScreen> {
   final AuthService _auth = AuthService();
+
+  // Widget que define el componente para hacer Login con Google
+  Widget _buildBotonOpcion(StatefulWidget route, Icon icon, String texto) {
+    return GestureDetector(
+      child: Container(
+        height: 120.0,
+        width: 120.0,
+        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.green[500],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          // image: DecorationImage(
+          //   image: logo,
+          // )
+        ),
+        child: FlatButton(
+          onPressed: () async {
+            //await _auth.signOut();
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => route
+              )
+            );
+            //Navigator.pop(context);
+          },
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            children: <Widget>[
+              icon,
+              Text(
+                texto,
+                style: new TextStyle(
+                  fontSize: 11.0,
+                  // color: Colors.yellow,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Widget que define el componente para hacer Login con Google
+  Widget _buildBotonOpcion2(StatefulWidget route, Icon icon, String texto) {
+    return GestureDetector(
+      child: Container(
+        height: 120.0,
+        width: 250.0,
+        margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.green[500],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+          // image: DecorationImage(
+          //   image: logo,
+          // )
+        ),
+        child: FlatButton(
+          onPressed: () async {
+            //await _auth.signOut();
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => route
+              )
+            );
+            //Navigator.pop(context);
+          },
+          padding: EdgeInsets.all(25.0),
+          child: Column( 
+            children: <Widget>[
+              icon,
+              Text(
+                texto,
+                style: new TextStyle(
+                  // fontSize: 10.0,
+                  // color: Colors.yellow,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  // Widget que define el componente para Login con Google
+  Widget  _buildOptionButtonsContainer(StatefulWidget route, Icon icon, String texto, StatefulWidget route2, Icon icon2, String texto2) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildBotonOpcion(
+            route,
+            icon,
+            texto,
+          ),
+          _buildBotonOpcion(
+            route2,
+            icon2,
+            texto2,
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Widget que define el componente para Login con Google
+  Widget  _buildOptionButtonsContainer2(StatefulWidget route, Icon icon, String texto) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildBotonOpcion2(
+            route,
+            icon,
+            texto
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +183,7 @@ class _MainScreenState extends State<MainScreen> {
         backgroundColor: Colors.green[50],
         appBar: AppBar(
           title: TextoNombreUsuario(),
-          backgroundColor: Color(0xFF00AB08),
+          backgroundColor: Color(0xFF149414),
           elevation: 0.0,
           actions: <Widget>[
             FlatButton.icon(
@@ -54,7 +191,63 @@ class _MainScreenState extends State<MainScreen> {
               padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
               label: Text(''),
               onPressed: () => _showSettingsPanel(),
-            ),           
+            ),
+          ],
+        ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 40.0,
+                  vertical: 40.0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    _buildOptionButtonsContainer(
+                      LoginScreen(),
+                      Icon(
+                        Icons.monetization_on,
+                        size: 45.0,
+                      ),
+                      'Presupuesto',
+                      LoginScreen(),
+                      Icon(
+                        Icons.poll,
+                        size: 45.0,
+                      ),
+                      'Estadísticas'
+                    ),
+                    _buildOptionButtonsContainer2(
+                      LoginScreen(),
+                      Icon(
+                        Icons.party_mode,
+                        size: 50.0,
+                      ),
+                      'Scaneo de Facturas'
+                    ),
+                    _buildOptionButtonsContainer(
+                      LoginScreen(),
+                      Icon(
+                        //Icons.store,
+                        Icons.local_atm,
+                        size: 45.0,
+                      ),
+                      'Bancos',
+                      LoginScreen(),
+                      Icon(
+                        Icons.room,
+                        size: 45.0,
+                      ),
+                      'Consultas'
+                    ),
+                  ],
+                )
+              ),
+            ),
           ],
         ),
       ),
